3862fd15d9b5ccef0a5417a07f21e6ce78ae0c63
Osman KUŞTU <64109752+osmankustu@users.noreply.github.com>
2026-01-22 03:05:49 +0300

    dfdsafsd

--- src/app/(public)/(auth)/signup/page.tsx
+++ src/app/(public)/(auth)/signup/page.tsx
@@ -1,13 +1,6 @@
 "use client";
-import { Metadata } from "next";
 
 import SignUpForm from "@/modules/IAM/auth/components/SignUpForm";
-
-export const metadata: Metadata = {
-  title: "Next.js SignUp Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js SignUp Page TailAdmin Dashboard Template",
-  // other metadata
-};
 
 export default function SignUp() {
   return <SignUpForm />;
